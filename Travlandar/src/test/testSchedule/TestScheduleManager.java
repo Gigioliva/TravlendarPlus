41fd989003257528b0f6567296f528cb4abc9a30
marcomussi <marco.mussi@mail.polimi.it>
2017-12-29 02:29:22 +0100

    schedule manager test

--- Travlandar/src/test/testSchedule/TestScheduleManager.java
+++ Travlandar/src/test/testSchedule/TestScheduleManager.java
@@ -1,5 +1,7 @@
 package test.testSchedule;
 
+import static org.junit.Assert.assertEquals;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Time;
@@ -16,41 +18,41 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import database.DataHandlerDBMS;
+import dati.Break;
 import dati.Event;
 import dati.EventType;
 import dati.Journey;
 import dati.Schedule;
 import dati.TypeMeans;
 import dati.User;
+import junit.framework.Assert;
 import schedule.ScheduleManager;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({DataHandlerDBMS.class, ScheduleManager.class})		
+@PrepareForTest({ DataHandlerDBMS.class, ScheduleManager.class })
 
 public class TestScheduleManager {
 
-	private User user;
-	
 	@Mock
 	private ResultSet resultSet;
-	private Schedule sched;
-	//ArrayList<Journey> j = new ArrayList<Journey>();
-	
-	
+
 	@Before
 	public void init() {
-		user = new User("username", "name", "surname", "mail", "phone", "drivinglicence", "creditcard", 1, new Time(3600000));
+
 	}
-	
+
 	@Test
 	public void testCreateSchedule() {
 		PowerMockito.mockStatic(DataHandlerDBMS.class);
 		PowerMockito.when(DataHandlerDBMS.executeDML(Matchers.anyString())).thenReturn(true);
+		User user = Mockito.mock(User.class);
+		ArrayList<Break> br = new ArrayList<Break>();
+		Mockito.when(user.getBreakPref()).thenReturn(br);
 		Boolean bool1 = new Boolean(false);
 		bool1 = ScheduleManager.createSchedule(user, new String("test"));
-		assert(bool1);
+		assert (bool1);
 	}
-	
+
 	@Test
 	public void testHasSchedule() {
 		PowerMockito.mockStatic(DataHandlerDBMS.class);
@@ -62,50 +64,101 @@ public class TestScheduleManager {
 		}
 		Boolean bool2 = new Boolean(false);
 		bool2 = ScheduleManager.hasSchedule("username", "test");
-		assert(bool2);
-	}	
-	
-	//sistemare, il test non copre il metodo completamente
-	@Test
-	public void testGetSchedule() {
-		PowerMockito.mockStatic(ScheduleManager.class);
-		PowerMockito.when(ScheduleManager.hasSchedule("username", "test")).thenReturn(true);
-		try {
-		Schedule schedule = ScheduleManager.getSchedule("username", "test");
-		assert(schedule.equals(null));
-		} catch (NullPointerException e) {
-		}
+		assert (bool2);
 	}
-	
+
 	@Test
-	public void testAddEvent() {
-		
+	public void testDeleteEvent() {
+		PowerMockito.mockStatic(DataHandlerDBMS.class);
+		PowerMockito.when(DataHandlerDBMS.executeDML(Matchers.anyString())).thenReturn(true);
+		Boolean bool3 = new Boolean(false);
+		bool3 = ScheduleManager.deleteEvent(123);
+		assert (bool3);
 	}
-	
+
 	@Test
 	public void testDeleteSchedule() {
 		PowerMockito.mockStatic(DataHandlerDBMS.class);
 		PowerMockito.when(DataHandlerDBMS.executeDML(Matchers.anyString())).thenReturn(true);
 		PowerMockito.mockStatic(ScheduleManager.class);
-		PowerMockito.when(ScheduleManager.getSchedule("username", "test")).thenReturn(sched);
-		ArrayList<Journey> j = new ArrayList<Journey>();
-		j.add(new Journey(new Time(0), new Time(3600000), TypeMeans.bicycling, 
+		Schedule sched = new Schedule("username", "01-01-2000");
+		sched.addJourney(new Journey(new Time(0), new Time(3600000), TypeMeans.bicycling,
 				new Event(100, "name", new Time(3600000), new Time(7200000), EventType.BREAK, "eventPosition"),
 				"testPosition"));
-		Schedule sched = Mockito.mock(Schedule.class);
-		Mockito.when(sched.getSchedule()).thenReturn(j);
+		PowerMockito.when(ScheduleManager.getSchedule("username", "test")).thenReturn(sched);
 		PowerMockito.when(ScheduleManager.deleteEvent(Matchers.anyInt())).thenReturn(true);
-		//Boolean bool3 = new Boolean(false);
-		//bool3 = ScheduleManager.deleteSchedule("username", "test");
-		//assert(bool3);
+		Boolean bool4 = new Boolean(false);
+		bool4 = ScheduleManager.deleteEvent(123);
+		assert (bool4);
 	}
-	
+
 	@Test
-	public void testDeleteEvent() {
+	public void testGetIntMax() {
 		PowerMockito.mockStatic(DataHandlerDBMS.class);
-		PowerMockito.when(DataHandlerDBMS.executeDML(Matchers.anyString())).thenReturn(true);
-		Boolean bool4 = ScheduleManager.deleteEvent(123);
-		assert(bool4);
+		PowerMockito.when(DataHandlerDBMS.sendQuery(Matchers.anyString())).thenReturn(resultSet);
+		try {
+			Mockito.when(resultSet.next()).thenReturn(true);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			Mockito.when(resultSet.getInt("Max")).thenReturn(12345);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		assertEquals(ScheduleManager.getIntMax(), 12346);
 	}
-	
+
+	// da completare
+
+	@Test
+	public void testGetSchedules() {
+		PowerMockito.mockStatic(DataHandlerDBMS.class);
+		PowerMockito.when(DataHandlerDBMS.sendQuery(Matchers.anyString())).thenReturn(resultSet);
+		try {
+			Mockito.when(resultSet.next()).thenReturn(true).thenReturn(false);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			Mockito.when(resultSet.getString(Matchers.anyString())).thenReturn("01-01-2000");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		PowerMockito.mockStatic(ScheduleManager.class);
+		PowerMockito.when(ScheduleManager.getSchedule(Matchers.anyString(), Matchers.anyString()))
+				.thenReturn(new Schedule("testUsername", "01-01-2000"));
+		Assert.assertNotNull(ScheduleManager.getSchedules("testUsername"));
+	}
+
+	@Test
+	public void testGetSchedule() {
+		PowerMockito.mockStatic(ScheduleManager.class);
+		PowerMockito.when(ScheduleManager.hasSchedule(Matchers.anyString(), Matchers.anyString()))
+				.thenReturn(true);
+		PowerMockito.mockStatic(DataHandlerDBMS.class);
+		PowerMockito.when(DataHandlerDBMS.sendQuery(Matchers.anyString())).thenReturn(resultSet);
+		try {
+			Mockito.when(resultSet.next()).thenReturn(false);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		//Assert.assertNotNull(ScheduleManager.getSchedule("testUsername","01-01-2000"));
+	}
+
+	@Test
+	public void testAddEvent() {
+		// Richiede ExternalRequestManager ch da un problema di compatibilità
+	}
+
+	@Test
+	public void testAddBreak() {
+
+	}
+
+	@Test
+	public void testcanAddBreak() {
+
+	}
+
 }
